9bdf5ce71d49fa0740eb482c37cbe0cb253b0908
Sandro <sandromedeirosdacunha@gmail.com>
2023-04-14 19:31:11 -0300

    Adicionando Model

---
+++ src/model/bo/ItemCompra.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bo;
+
+/**
+ *
+ * @author aluno
+ */
+public class ItemCompra {
+    private int id;
+    private float qntProduto, valorUnitario;
+    private char status;
+    private Compra compra;
+    private Produto produto;
+
+    public ItemCompra() {
+    }
+    
+    
+    
+    
+}
